dbde3e5bbd03dd2667ef77b28ce89a3fa093c0d9
od507 <antonio.daniels@utp.ac.pa>
2023-06-01 18:54:14 -0500

    verificando como usar GIT

--- src/ventanasiBiblioteca/Principal.java
+++ src/ventanasiBiblioteca/Principal.java
@@ -341,6 +341,7 @@ public class Principal extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Principal().setVisible(true);
+                
             }
         });
     }
